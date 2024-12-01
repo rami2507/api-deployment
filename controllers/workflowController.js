41bf373910c5c485aed6e8d85a2b416f3f93f1a9
Rami <lalouirami34@gmail.com>
2024-12-01 15:38:35 +0100

    update

--- controllers/workflowController.js
+++ controllers/workflowController.js
@@ -34,7 +34,7 @@ const getWorkflows = asyncHandler(async (req, res, next) => {
 
 const getOneWorkflow = asyncHandler(async (req, res, next) => {
   const workflowId = req.params.workflowId;
-  const workflow = await Workflow.findById(workflowId).populate("questions");
+  const workflow = await Workflow.findById(workflowId).populate("faqs");
 
   if (!workflow) {
     return next(new AppError("error fetching the workflow", 404));
