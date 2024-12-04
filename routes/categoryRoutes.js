13448b29de3aa85249aae2e4ce189bfccf449555
Rami <lalouirami34@gmail.com>
2024-12-04 18:19:29 +0100

    patch method added

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -5,6 +5,7 @@ const {
   createCategory,
   getCategory,
   deleteCategory,
+  updateCategory,
 } = require("./../controllers/categoryController");
 const router = express.Router();
 
@@ -12,6 +13,8 @@ router.get("/get-categories", getCategories);
 router.get("/get-one-category/:id", getCategory);
 
 router.post("/create-category", createCategory);
+
+router.patch("/update-category/:id", updateCategory);
 
 router.delete("/delete-categories", deleteCategories);
 router.delete("/delete-one-category/:id", deleteCategory);
