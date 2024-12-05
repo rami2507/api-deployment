843f3456b39df6fa6cd511f6f815b0fad5f459b2
Rami <lalouirami34@gmail.com>
2024-12-06 00:28:23 +0100

    last

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,9 +7,9 @@ const {
 const { validatorMiddleware } = require("./../middlewares/validatorMiddleware");
 const router = express.Router();
 
+router.get("/checkAuthenticated", protect);
 router.post("/signup", signupValidator, validatorMiddleware, signup);
 router.post("/login", login);
-router.get("/isAuthenticated", protect);
 
 router.get("/get-users", getUsers);
 router.delete("/delete-users", deleteUsers);
